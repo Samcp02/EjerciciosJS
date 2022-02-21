680e9b917c00f2e86d7699c7758115bedf3763fc
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-21 13:23:54 +0100

    exercise5 solved

--- src/for and if/exercise5/index.js
+++ src/for and if/exercise5/index.js
@@ -2,5 +2,10 @@ const numbers = [0,1,5,2,7,5,0,0,9,5,5,6];
 let counter = 0;
 
 // Put your code here
+for (let item of numbers){
+    if (item === 5) {
+        console.log(counter += 1);
+    }
+}
 
-console.log(counter)
+console.log('Number of 5: ' + counter)

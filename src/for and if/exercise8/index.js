692bb6f3ca829b9d78d626ad27b5283a693b6052
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-22 13:25:24 +0100

    exercise8 solved

--- src/for and if/exercise8/index.js
+++ src/for and if/exercise8/index.js
@@ -1,1 +1,6 @@
 // Put your code here
+
+for (let outer = 1; outer < 10 ; outer++) {
+    for (let inner = 1; inner < 10; inner++ ){
+        console.log(`${outer} x ${inner} = ` + outer * inner)}
+}

5c3dbfd8d85a4744800a9436711ce88164e0390e
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-25 13:42:13 +0100

    changes exercises(2,3,4,5)

--- src/functions/exercise5/index.js
+++ src/functions/exercise5/index.js
@@ -1,7 +1,14 @@
 const numbers = [11,9,13,12]
 
 // Put your code here
-
+function minorInRange (a,b) {
+    let minor = numbers[0];
+    if (numbers[a] < numbers[b] {
+         minor = numbers[a]
+        } else {minor = numbers[b]}
+          
+    }
+return minor;
 console.log(minorInRange(0,3,numbers));
 console.log(minorInRange(2,3,numbers));
 console.log(minorInRange(1,3),numbers);

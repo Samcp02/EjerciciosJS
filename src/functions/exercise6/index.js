87e04154c8638e82b5eb623f7ca90dbd484cecec
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-02 13:42:45 +0100

    exercise6 solved

--- src/functions/exercise6/index.js
+++ src/functions/exercise6/index.js
@@ -1,7 +1,6 @@
 const numbers = [11,9,13,12]
 
 // Paste your functions here
-
 //Función para elegir el menor entre un rango
 
 function minorInRange (idx1, idx2) {
@@ -9,20 +8,20 @@ function minorInRange (idx1, idx2) {
   let minor;
 
   for (let i = idx1; i <= idx2; i++) {
-    if (numbers[idx1] < numbers[idx1+1]) {
-         minor = numbers[idx1]
-        } else {minor = numbers[idx1+1]}
-    }
-    return minor;
+    if (numbers[idx1] < numbers[idx1 + 1]) {
+      minor = idx1
+    } else { minor = idx1 + 1 }
   }
+  return minor;
+}
     
 // Función para intercambiar dos números de un array
 
-    function swap (idx1, idx2) {
-      let temp = numbers[idx2];
-      numbers[idx2] = numbers[idx1];
-      numbers[idx1] = temp;
-      };
+function swap(idx1, idx2) {
+  let temp = numbers[idx2];
+  numbers[idx2] = numbers[idx1];
+  numbers[idx1] = temp;
+};
 
 // Put your code here
-console.log(minorInRange(0,3,numbers))
+console.log(minorInRange(0,3,numbers))  

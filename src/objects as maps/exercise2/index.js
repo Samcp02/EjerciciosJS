595efba909f9620fd45950acd2ad9f5b6029ff19
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-03-16 12:49:25 +0100

    exercise2 done

--- src/objects as maps/exercise2/index.js
+++ src/objects as maps/exercise2/index.js
@@ -22,19 +22,6 @@ const chrono = [
 ]
 
 // Put you code here
-function sort () {
-    let currentPosition= currentPosition.time
-    let compareWithPosition= compareWithPosition.time
 
-    for (let currentPosition = 0; currentPosition < chrono.length ; currentPosition++) {
-        for (let compareWithPosition = currentPosition+1; compareWithPosition < chrono.length; compareWithPosition++)
-        {
-            if (chrono[currentPosition] > chrono[compareWithPosition]) {
-                let temporal = chrono[currentPosition];
-                chrono[currentPosition] = chrono[compareWithPosition];
-                chrono[compareWithPosition] =  temporal;
-                console.log(chrono)
-            }
-        }
-    }
-}
+chrono.sort(function (a,b) {return a.time - b.time})
+console.log(JSON.stringify(chrono))
